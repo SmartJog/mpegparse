659996354ff37cdc30081eebe8ed3c6b2658a172
Niv Sardi <nsardi@smartjog.com>
2010-05-07 15:51:03 +0200

    flvtool: make it work, using icecast pluging logic
    Signed-off-by: Niv Sardi <nsardi@smartjog.com>

--- flvtool.c
+++ flvtool.c
@@ -1,17 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #include "libxa1dump.h"
 #include "flv.h"
 
+typedef struct refbuf refbuf_t;
+struct refbuf {
+	char		*data;
+	ssize_t		len;
+};
+
+refbuf_t *refbuf_new (size_t size) {
+	refbuf_t *ret = malloc(sizeof(refbuf_t));
+
+	if (!ret)
+		return NULL;
+
+	ret->data = malloc(size);
+	if (!ret->data) {
+		free (ret);
+		return NULL;
+	}
+	ret->len = size;
+
+	return ret;
+}
+
+refbuf_t *refbuf_realloc (refbuf_t *refbuf, size_t newsize) {
+	char *newdata;
+
+	if (!refbuf)
+		return refbuf_new (newsize);
+
+	newdata = realloc(refbuf->data, newsize);
+	if (!newdata)
+		return NULL;
+
+	refbuf->len = newsize;
+	refbuf->data = newdata;
+
+	return refbuf;
+}
+
+void refbuf_destroy (refbuf_t *refbuf) {
+	free(refbuf->data);
+	free(refbuf);
+}
+
 int main (int argc, char *argv[]) {
-	int 	fd;
-	size_t	len, tot = 0;
-	char	buf[4096];
-	char	*p = buf, *q = NULL;
-	int i;
+	int 		fd, bytes;
+	ssize_t		len;
+	size_t		offset = 0, readc = 0;
+
+	refbuf_t	*refbuf;
+	parseme_t	*h, *t;
 
 	if (argc < 2) {
 		perror ("Need more arguments");
@@ -24,21 +72,60 @@ int main (int argc, char *argv[]) {
 		return 0;
 	}
 
-	for (;(len = read (fd, buf, sizeof(buf)) != 0); tot += len) {
-		xa1_dumpa(tot, p);
-		if (parse((char *) p, len, flv_header, &q))
-			break;
+	h = mpegparse_new (flv_header);
+	t = mpegparse_new (flv_tag);
+
+	refbuf = refbuf_new (4096);
+
+	if (!h || !t || !refbuf) {
+		perror ("memory issue");
+		return 0;
 	}
-			tot += q - p;
-			p = q;
 
-	do {
-		xa1_dumpa(tot, p);
-		while( parse((char *) p, sizeof(buf), flv_tag, &q)) {
-			tot += q - p;
-			p = q;
+	refbuf->len = read (fd, refbuf->data, refbuf->len); /* not really ideal */
+	bytes = parse (refbuf->data, refbuf->len, h, NULL);
+	if (bytes <= 0)
+		goto err_parse;
 
-			xa1_dumpa(tot, p);
+	readc += bytes;
+
+	while (1) {
+		while ((bytes = parse(refbuf->data + readc, refbuf->len - readc, t, NULL)) > 0) {
+			if (refbuf->len - bytes - readc < FLV_TAG_SIZE) {
+				printf("buffer %d too small, bail out\n", refbuf->len - readc);
+				bytes = -bytes + FLV_TAG_SIZE;
+				break;
+			}
+			readc += bytes;
 		}
-	}while (len = read (fd, buf, sizeof(buf)) != 0);
+		if  (!bytes) {
+			printf("Couldn't parse FLV Metadata at %d, %s\n",
+			       readc, refbuf->data + readc);
+			goto err_parse;
+		}
+
+
+		offset = refbuf-> len - readc;
+		memmove (refbuf->data, refbuf->data + readc, offset);
+		if (bytes + refbuf->len < 0)
+			refbuf = refbuf_realloc (refbuf, -bytes + FLV_TAG_SIZE);
+
+		for (readc = 0; readc < refbuf->len;) {
+			bytes = read (fd, refbuf->data + offset, refbuf->len);
+			if (bytes)
+				readc += bytes;
+			else
+				goto err_readc;
+		}
+		readc = 0;
+	}
+
+ err_parse:
+	printf ("ERROR: Couldn't parse (bytes = %d)\n", bytes);
+	xa1_dump_size (refbuf->data, refbuf->len);
+	return 0; /* need to free */
+
+ err_readc:
+	printf ("ERROR while reading %m\n");
+	return 0;
 }
